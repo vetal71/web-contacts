2ee2a051b503ff5eba1d0de787153e03ffd90320
Vitaliy Kolesnik <vetalkolesnik@gmail.com>
2016-04-29 23:15:56 +0300

    Добавлен метод в модель Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
 	# модель класса Contact	
+	attr_accessible :fio, :sex, :currentnotes, :region, :city,
+		:birthdate, :celurarphone, :homephone, :email, :othertypelinks,
+		:address, :passport, :specialization, :transfertype, :numbercard,
+		:memberprojects, :datelast, :countanketa, :issupervizer,
+		:percentgood, :percentbad, :generalcharacteristic
 end
